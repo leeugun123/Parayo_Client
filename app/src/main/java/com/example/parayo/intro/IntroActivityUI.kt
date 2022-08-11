7b1c427d9a6944358d618fb2188003a910894233
이유건 <50404123+leeugun123@users.noreply.github.com>
2022-08-11 17:04:19 +0900

    retrofit 라이브러리 추가

--- app/src/main/java/com/example/parayo/intro/IntroActivityUI.kt
+++ app/src/main/java/com/example/parayo/intro/IntroActivityUI.kt
@@ -1,8 +1,30 @@
 package com.example.parayo.intro
 
-import org.jetbrains.anko.AnkoComponent
+import android.graphics.Typeface
+import android.view.Gravity
+import android.view.View
+import com.example.parayo.R
+import org.jetbrains.anko.*
 
 class IntroActivityUI : AnkoComponent<IntroActivity> {
+
+    override fun createView(ui: AnkoContext<IntroActivity>): View {
+
+
+        return ui.relativeLayout{
+            gravity = Gravity.CENTER
+
+            //PARAYO라는 텍스트 출력
+
+            textView("PARAYO"){
+                textSize = 24f
+                textColorResource = com.google.android.material.R.color.design_dark_default_color_primary
+                typeface = Typeface.DEFAULT_BOLD
+            }
+
+
+        }
+    }
 
 
 }
