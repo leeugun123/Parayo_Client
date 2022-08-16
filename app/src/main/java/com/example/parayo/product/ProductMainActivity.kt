4c02ebefd49a91d22047e8acd8b9f07124c2c6ab
이유건 <50404123+leeugun123@users.noreply.github.com>
2022-08-16 20:43:25 +0900

    툴바 추가

--- app/src/main/java/com/example/parayo/product/ProductMainActivity.kt
+++ app/src/main/java/com/example/parayo/product/ProductMainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.parayo.product
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.parayo.R
 import net.codephobia.ankomvvm.components.BaseActivity
 import org.jetbrains.anko.setContentView
+import org.jetbrains.anko.support.v4.drawerLayout
+import org.jetbrains.anko.toolbar
 import kotlin.reflect.KClass
 
 class ProductMainActivity : BaseActivity<ProductMainViewModel>(){
@@ -14,6 +18,23 @@ class ProductMainActivity : BaseActivity<ProductMainViewModel>(){
         ProductMainUI(getViewModel())
             .setContentView(this)
 
+        setupDrawerListener()
+
+    }
+
+    private fun setupDrawerListener() {
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout(),
+            //ui.toolBar
+            R.string.open_drawer_description,
+            R.string.close_drawer_descripton
+        )
+
+        drawerLayout().addDrawerListener(toggle)
+
+        toggle.syncState()
     }
 
 
--- app/src/main/java/com/example/parayo/product/ProductMainUI.kt
+++ app/src/main/java/com/example/parayo/product/ProductMainUI.kt
@@ -1,6 +1,10 @@
 package com.example.parayo.product
 
 import android.view.Gravity
+import android.view.MenuItem.SHOW_AS_ACTION_ALWAYS
+import android.widget.Toolbar
+import com.example.parayo.R
+import com.example.parayo.view.borderBottom
 import org.jetbrains.anko.*
 import org.jetbrains.anko.AnkoComponent
 import org.jetbrains.anko.AnkoContext
@@ -9,10 +13,23 @@ import org.jetbrains.anko.support.v4.drawerLayout
 
 class ProductMainUI(private val viewModel: ProductMainViewModel) : AnkoComponent<ProductMainActivity> {
 
+    lateinit var toolbar: Toolbar
+
+
     override fun createView(ui: AnkoContext<ProductMainActivity>) =
 
         ui.drawerLayout {
+
             verticalLayout {
+              toolbar =  toolbar{
+                    title = "Parayo"
+                    bottomPadding = dip(1)
+                    background = borderBottom(width = dip(1))
+                    menu.add("Search")
+                        .setIcon(R.drawable.ic_baseline_search)
+                        .setShowAsAction(SHOW_AS_ACTION_ALWAYS)
+
+                }.lparams(matchParent, wrapContent)
             }.lparams(matchParent, matchParent)
 
 
