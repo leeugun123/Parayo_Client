1bd16dcc40c90953663b820e88a5077de4ef5922
이유건 <50404123+leeugun123@users.noreply.github.com>
2022-08-22 13:40:29 +0900

    상품 리스트 UI

---
+++ app/src/main/java/com/example/parayo/product/ProductStatus.kt
@@ -0,0 +1,7 @@
+package com.example.parayo.product
+
+object ProductStatus {
+
+    val SELLABLE  = "sellable"
+    val SOLD_OUT = "SOLD_OUT"
+}
--- app/src/main/java/com/example/parayo/product/list/ProductListViewModel.kt
+++ app/src/main/java/com/example/parayo/product/list/ProductListViewModel.kt
@@ -1,11 +1,41 @@
 package com.example.parayo.product.list
 
 import android.app.Application
+import android.content.Intent
+import androidx.paging.DataSource
+import com.example.parayo.api.response.ProductListItemResponse
 import net.codephobia.ankomvvm.lifecycle.BaseViewModel
+import org.jetbrains.anko.error
+import java.lang.IllegalStateException
 
-class ProductListViewModel(app: Application) : BaseViewModel(app){
+class ProductListViewModel(app: Application) : BaseViewModel(app) , ProductListPagedAdapter.ProductLiveDataBuilder,
+ProductListPagedAdapter.OnItemClickListener{
 
-    fun onClickItem(id: Long?){
-        toast("click $id")
+    var categoryId : Int = -1
+    val products = buildPageList()
+
+    override fun createDataSource(): DataSource<Long, ProductListItemResponse> {
+
+        if(categoryId == -1) {
+            error(
+                "categoryId가 설정되지 않았습니다.",
+                IllegalStateException("categoryId is -1")
+            )
+        }
+
+
+        return ProductListItemDataSource(categoryId)
+
     }
+
+    override fun onClickProduct(productId: Long?) {
+
+        startActivity<ProductDetailActiviy>{
+            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+            putExtra(ProductDetailActiviy.PRODUCT_ID,productId)
+        }
+
+    }
+
+
 }
